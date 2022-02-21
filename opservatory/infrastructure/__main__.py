a91c907452fb584938cab5d56579f3921bfe60cb
Vladimir Semenov <sepsseven@gmail.com>
2022-02-21 10:44:09 +0300

    Update __main__.py

--- opservatory/infrastructure/__main__.py
+++ opservatory/infrastructure/__main__.py
@@ -1,5 +1,8 @@
-from infrastructure.ansible import AnsibleInfrastructureCommunicator
+from opservatory.infrastructure.ansible import AnsibleInfrastructureCommunicator
 
 
 comm = AnsibleInfrastructureCommunicator()
-print(comm.update_machines_info())
+# print(comm.update_machines_info())
+# print(comm.list_docker_containers('mmz3'))
+# comm.gather_facts()
+# print(comm.update_machines_info())
