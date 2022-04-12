97d95dc32ad8f7caf8a7a275d41e70988f15dd0d
Vladimir Semenov <sepsseven@gmail.com>
2022-04-12 12:10:52 +0300

    Ansible tmp cleaning

--- opservatory/scheduler.py
+++ opservatory/scheduler.py
@@ -3,14 +3,16 @@ import os
 from pathlib import Path
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+import shutil
 
-from opservatory.app import update_containers_info, update_fleet_facts, save_fleet
+from opservatory.app import update_containers_info, update_fleet_facts
 from opservatory.infrastructure.ansible import AnsibleInfrastructureCommunicator
 from opservatory.state.json_repo import JsonStateRepository
 
 logger = logging.getLogger("main")
 sched = BackgroundScheduler()
 
+TEMP_FILES_PATH = Path(os.path.dirname(__file__)) / 'infrastructure' / 'ansible' / 'tmp'
 PROJECT_PATH = Path(os.path.dirname(__file__))
 STATE_DUMP_PATH = PROJECT_PATH / Path('state.json')
 
@@ -21,6 +23,9 @@ def update_fleet():
     comm = AnsibleInfrastructureCommunicator()
     repo = JsonStateRepository(path=STATE_DUMP_PATH)
     fleet = update_fleet_facts(comm, repo)
+
+    shutil.rmtree(TEMP_FILES_PATH)
+
     print("Machines updated:", len(fleet.machines))
 
 
